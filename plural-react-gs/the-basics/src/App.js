a697415c814269c4e7511fac0a0c256135b7147e
Swetank <iamswetank@gmail.com>
2019-08-13 04:57:18 +0530

    :dizzy: Add Chapter2

--- plural-react-gs/the-basics/src/App.js
+++ plural-react-gs/the-basics/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Ch2 from "./Ch2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello React! by Swetank Raj.</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/swetankraj/react-experiments/tree/master/plural-react-gs/the-basics"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fork on GitHub
         </a>
+        <p>
+          This repo cumulates the learning of{" "}
+          <a
+            className="App-link"
+            href="https://app.pluralsight.com/library/courses/react-js-getting-started"
+          >
+            React: Getting Started
+          </a>{" "}
+          by Samer Buna.
+        </p>
+        <Ch2 />
       </header>
     </div>
   );
---
+++ plural-react-gs/the-basics/src/Ch2.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button>Test!</button>
+    </div>
+  );
+}
+
+function Ch2() {
+  return (
+    <div>
+      <h2>Chapter 2: The Basics</h2>
+      <Button />
+    </div>
+  );
+}
+
+export default Ch2;
