bc7a77f4df7fe7f6925c50f50221f7e690bc5fa0
Swetank <iamswetank@gmail.com>
2019-08-13 06:05:34 +0530

    :cyclone: add two components

--- plural-react-gs/the-basics/src/Ch2.js
+++ plural-react-gs/the-basics/src/Ch2.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Button() {
+// Part a of the app.
+function PartA() {
+  const [counter, setCounter] = useState(10);
+  const handleClick = () => setCounter(counter + 1);
   return (
     <div>
-      <button>Test!</button>
+      <p>
+        <p>a. Incrementer Button</p>
+      </p>
+      <button onClick={handleClick}>{counter}</button>
     </div>
   );
 }
 
+// Part b of the app.
+function PartBDisp(props) {
+  return (
+    <div>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+function PartBButt(props) {
+  return <button onClick={props.update}>+1</button>;
+}
+
+function PartB() {
+  const [counter, setCounter] = useState(10);
+  const handleClick = () => setCounter(counter + 1);
+  return (
+    <div>
+      <p>b. View Incrementer Button</p>
+      <PartBDisp message={counter} />
+      <PartBButt update={handleClick} />
+    </div>
+  );
+}
+
+//Global app.
 function Ch2() {
   return (
     <div>
       <h2>Chapter 2: The Basics</h2>
-      <Button />
+      <PartA />
+      <PartB />
     </div>
   );
 }
